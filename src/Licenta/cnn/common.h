7e31bbfb61946be2b2b1d6bb95fe2fceca021f71
Raul <puscas.raul.pr@gmail.com>
2025-09-04 20:03:45 +0300

    interface for common activation functions

--- src/Licenta/cnn/common.h
+++ src/Licenta/cnn/common.h
@@ -1,3 +1,5 @@
+#include <math.h>
+
 #ifndef CNN_COMMON_H
 #define CNN_COMMON_H
 
@@ -6,15 +8,37 @@ typedef enum {
   ACT_SIG,
 } ACT_FUNC;
 
-float sigmoidf(float x);
-double sigmoidd(double x);
-int sigmoidi(int x);
-long sigmoidli(long x);
+static inline float sigmoidf(float x) {
+  return x >= 0? (1.f / (1.f + exp(-x))) : (exp(x) / (1.f + exp(x)));
+}
 
-float reluf(float x); 
-double relud(double x);
-int relui(int x);
-long reluli(long x);
+static inline double sigmoidd(double x) {
+  return x >= 0? (1.f / (1.f + exp(-x))) : (exp(x) / (1.f + exp(x)));
+}
+
+static inline int sigmoidi(int x) {
+  return x >= 0? (1.f / (1.f + exp(-x))) : (exp(x) / (1.f + exp(x)));
+}
+
+static inline long sigmoidli(long x) {
+  return x >= 0? (1.f / (1.f + exp(-x))) : (exp(x) / (1.f + exp(x)));
+}
+
+static inline float reluf(float x) {
+  return x > 0 ? x : x * 0.1f;
+}
+
+static inline double relud(double x) {
+  return x > 0 ? x : x * 0.1f;
+}
+
+static inline int relui(int x) {
+  return x > 0 ? x : x * 0.1f;
+}
+
+static inline long reluli(long x) {
+  return x > 0 ? x : x * 0.1f;
+}
 
 #define sigmoid(X) \
 _Generic((X), \
