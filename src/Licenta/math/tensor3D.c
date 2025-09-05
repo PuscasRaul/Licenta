e901e1f335bf833628206a3b78eef95afb8957e7
Raul <puscas.raul.pr@gmail.com>
2025-09-05 20:33:33 +0300

    dropped the [[deprecated tag]], created activation function over the whole tensor

--- src/Licenta/math/tensor3D.c
+++ src/Licenta/math/tensor3D.c
@@ -3,7 +3,6 @@
 #include "../utils/arena.h"
 #include "Mat.h"
 
-[[deprecated("Implementation")]]
 Tensor3D *tensor_init(
     Tensor3D tensor[static 1],
     size_t size,
@@ -16,28 +15,15 @@ Tensor3D *tensor_init(
   *tensor = (Tensor3D) {
     .size = size, 
     .depth = depth,
-    .maps = mat_create_array(depth)
+    .maps = mat_vcreate(depth, size)
   };
 
   if (!tensor->maps) 
     return nullptr;
   
-  for (size_t i = 0; i < depth; i++) 
-    if (!mat_init(&tensor->maps[i], size, size)) 
-      goto fail;
-  
   return tensor;
-
-fail:
-  for (size_t i = 0; i < depth; i++) {
-    if (!tensor->maps[i].rows)
-      break;
-    mat_deinit(&tensor->maps[i]);
-  }
-  return nullptr; 
 }
 
-[[deprecated("Implementation")]]
 void tensor_deinit(Tensor3D tensor[static 1]) {
   for (size_t i = 0; i < tensor->depth; i++) 
      mat_deinit(&tensor->maps[i]);
@@ -46,3 +32,41 @@ void tensor_deinit(Tensor3D tensor[static 1]) {
   *tensor = (Tensor3D) {0};
 }
 
+[[nodiscard]]
+Tensor3D *tensor_new(size_t size, size_t depth) {
+  return tensor_init(malloc(sizeof(Tensor3D)), size, depth);
+}
+
+void tensor_destroy(Tensor3D *t) {
+  if (t) {
+    tensor_deinit(t);
+    free(t);
+  }
+}
+
+void activate_tensor(Tensor3D *tensor, ACT_FUNC activation) {
+  switch (activation) {
+    case ACT_RELU:
+      for (size_t k = 0; k < tensor->depth; k++) {
+        for (size_t i = 0; i < tensor->size; i++) {
+          for (size_t j = 0; j < tensor->size; j++) {
+            float val = *(mat_at(&tensor->maps[k], i, j));
+            *(mat_at(&tensor->maps[k], i, j)) = relu(val);
+          }
+        }
+      }
+      break;
+    case ACT_SIG:
+      for (size_t k = 0; k < tensor->depth; k++) {
+        for (size_t i = 0; i < tensor->size; i++) {
+          for (size_t j = 0; j < tensor->size; j++) {
+            float val = *(mat_at(&tensor->maps[k], i, j));
+            *(mat_at(&tensor->maps[k], i, j)) = sigmoid(val);
+          }
+        }
+      }
+      break;
+    default:
+      break;
+  }
+}
--- src/Licenta/math/tensor3D.h
+++ src/Licenta/math/tensor3D.h
@@ -1,27 +1,30 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include "Mat.h"
+#include "activation.h"
 
 #ifndef TENSOR3D_H
 #define TENSOR3D_H
 
-typedef struct {
-  [[deprecated("Private")]] size_t size; // matrix size
-  [[deprecated("Private")]] uint8_t depth; // number of matrixes
-  [[deprecated("Private")]] Mat *maps; // data
-} Tensor3D;
+typedef struct Tensor3D Tensor3D;
+
+struct Tensor3D{
+  size_t size; // matrix size
+  uint8_t depth; // number of matrixes
+  Mat *maps; // data
+};
 
 /* Initialize a tensor buffer t with depth matrixes of size X size
  * Only use this function on an unitialized tensor
  * Each tensor initialized with this must be destroyed with
  * tensor_deinit
  */
-Tensor3D *tensor_init(Tensor3D t[static 1], size_t size, size_t depth);
+Tensor3D *tensor_init(Tensor3D *t, size_t size, size_t depth);
 
 /* Destroy Tensor3D buffer t
  * t must have been initialized with tensor_init
  */
-void tensor_deinit(Tensor3D t[static 1]);
+void tensor_deinit(Tensor3D *t);
 
 /* Create tensor buffer with depth matrixes of size X size
  * Each tensor initialized with this must be destroyed with
@@ -29,18 +32,13 @@ void tensor_deinit(Tensor3D t[static 1]);
  */
 
 [[nodiscard]]
-static inline Tensor3D *tensor_new(size_t size, size_t depth) {
-  return tensor_init(malloc(sizeof(Tensor3D)), size, depth);
-}
+Tensor3D *tensor_new(size_t size, size_t depth); 
 
 /* Destroy Tensor3D buffer t
  * t must have been created with tensor_new
  */
-void tensor_destroy(Tensor3D *t) {
-  if (t) {
-    tensor_deinit(t);
-    free(t);
-  }
-}
+void tensor_destroy(Tensor3D *t);
+
+void activate_tensor(Tensor3D *tensor, ACT_FUNC activation);
 
 #endif // TENSOR3D_H
