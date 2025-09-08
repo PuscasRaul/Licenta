f004d521f1de946cf1768db769465127a1c7e397
Raul <puscas.raul.pr@gmail.com>
2025-09-08 19:23:36 +0300

    added helper functions for heap allocation of convolution layer

--- src/Licenta/cnn/convolution.h
+++ src/Licenta/cnn/convolution.h
@@ -20,8 +20,33 @@ Convolution_Layer *conv_init(
     size_t stride,
     ACT_FUNC act_func
 );
-
 void conv_deinit(Convolution_Layer *cl);
+
+[[nodiscard]]
+static inline Convolution_Layer *conv_new(
+    size_t n_filters, 
+    size_t f_size,
+    size_t f_depth,
+    size_t stride,
+    ACT_FUNC act_func
+) {
+  return conv_init(
+    malloc(sizeof(Convolution_Layer)), 
+    n_filters, 
+    f_size,
+    f_depth,
+    stride,
+    act_func
+  );
+}
+
+static inline void conv_destroy(Convolution_Layer *cl) {
+  if (cl) {
+    conv_deinit(cl);
+    free(cl);
+  }
+}
+
 Tensor3D *get_activation_maps(Convolution_Layer *layer, Tensor3D *input); 
 
 #endif // CNN_CONVOLUTION_H
