d7bea9726a820ec5dee77408cc8a373e63ca10e8
raul <puscas.raul.pr@gmail.com>
2026-02-24 22:40:16 +0200

    added first implementation for veda

--- src/filters.py
+++ src/filters.py
@@ -45,3 +45,48 @@ class ProcessingFunctions():
         kernel = cv.getStructuringElement(cv.MORPH_RECT, (1, kernel_height))
         cleaned_array = cv.morphologyEx(array, cv.MORPH_OPEN, kernel)
         return cleaned_array
+
+    @staticmethod
+    def VEDA(array: np.array) -> np.array:
+        '''
+        Perform VEDA on a 2D binarized array
+        :param array: 2D binarized array
+
+        :return: 2D binarized array after VEDA has been applied
+        '''
+
+        # TODO: Finish this function tomorrow
+        veda_array = np.zeros_like(array)
+        it = np.nditer(array, flags=['multi_index'])
+        for coord in it:
+
+            row = coord[0]
+            col = coord[1]
+            left_col = coord[1] - 1
+            right_col = coord[1] + 1
+            right_right_col = coord[1] + 2
+            down_row = coord[0] + 1
+
+            if left_col < 0:
+                left_col = 0
+            if right_col >= np.shape(array)[1]:
+                right_col = col
+            if right_right_col >= np.shape(array)[1]:
+                right_right_col = right_col
+            if down_row >= np.shape(array)[0]:
+                down_row = row
+
+            center_cond = array[row][col] == 0 \
+                and array[row][right_col] == 0\
+                and array[down_row][col] == 0 \
+                and array[down_row][right_col] == 0
+            left_cond = array[row][left_col] == 0 \
+                and array[down_row][left_col] == 0
+            right_cond = array[down_row][right_right_col] == 0 \
+                and array[down_row][right_right_col] == 0
+
+            if not center_cond and not left_cond and not right_cond:
+                veda_array[coord[0]][coord[1]] = 255
+                veda_array[coord[0]][coord[1]+1] = 255
+
+        return veda_array
